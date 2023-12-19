6c2170d6366a775d3320bd196dc918fe4877b6ac
harryhuayuanchong <harryhuayuanchong@gmail.com>
2023-12-19 19:40:48 +0800

    chore: Frontend Testing Framework setup

---
+++ frontend/src/test/exampleTest.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// test/exampleTest.js
+/* eslint-disable no-unused-expressions */
+// eslint-disable-next-line no-undef
+const chai = require('chai');
+const expect = chai.expect;
+
+// eslint-disable-next-line no-undef
+describe('Example Test', () => {
+    // eslint-disable-next-line no-undef
+    it('should pass this test', () => {
+        expect(true).to.be.true;
+    });
+});
